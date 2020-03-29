1b7018d1c8ce178ad8c3ca1271ad1a6c9ea0d1ba
Jackie <JGtheophila19@gmail.com>
2020-03-29 14:37:05 -0400

    create a class to blueprint to a new bulldozer

--- Bulldozer/Bulldozer/Program.cs
+++ Bulldozer/Bulldozer/Program.cs
@@ -8,5 +8,14 @@ namespace Bulldozer
         {
             Console.WriteLine("Hello World!");
         }
+        
+        public class BullDozer
+        { 
+            // 2 properties
+
+            // define a color and size
+            public string color { get; set;}
+            public string size { get; set; }
+        }
     }
 }
