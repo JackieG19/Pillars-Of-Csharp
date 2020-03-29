adf137677b56feba0c75b0c11d8108e9b4d23efb
Jackie <JGtheophila19@gmail.com>
2020-03-29 18:29:09 -0400

    count the number of things and...
    store into a class variable

--- Bulldozer/Bulldozer/Program.cs
+++ Bulldozer/Bulldozer/Program.cs
@@ -10,9 +10,25 @@ namespace Bulldozer
 
         public class Workers
         {   
-        public Workers()
-        {
-            // constructor
+            public Workers()
+            {   
+                // constructor
+            }
         }
+        // worker count the number of things
+        public void Count(int number)
+        {
+            // if total is equal to null is not ready yet
+            if (Total == null)
+                Total = 0;
+           
+
+            Total = Total + number;
+            Console.WriteLine(Total.ToString());
+
+        }
+
+        // to store the number and add it, create a class variable
+        public int Total { get; set; }
     }
 }
