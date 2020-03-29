4111d8911a1b2e78234be44b8c66e809f1949bb6
Jackie <JGtheophila19@gmail.com>
2020-03-29 18:25:59 -0400

    creating a new object

--- Bulldozer/Bulldozer/Program.cs
+++ Bulldozer/Bulldozer/Program.cs
@@ -6,70 +6,13 @@ namespace Bulldozer
     {
         public static void Main(string[] args)
         {
-            var man = new Man();
-            var kart = new Kart();
-            var alien = new Alien();
         }
 
-        public class GeneralObject
+        public class Workers
+        {   
+        public Workers()
         {
-            public GeneralObject()
-            {
-                // constructor
-            }
-            // common properties
-            public bool move { get; set; }
-            public string color { get; set; }
-        }
-
-       // Inheritance the properties from GeneralObject
-        public class Human : GeneralObject
-        {
-            public Human()
-            {
-                // constructor
-            }
-
-            public string name { get; set; }
-        }
-
-        // will Inheritance 2 classes
-        public class Man : Human
-        {
-            public Man ()
-            {
-                // constructor
-            }
-
-            //public string name { get; set; }
-        }
-
-        public class Woman : Human
-        {
-            public Woman ()
-            {
-                // constructor
-            }
-
-            //public string name { get; set; }
-        }
-
-        public class Kart
-        {
-            public Kart()
-            {
-                // constructor
-            }
-            public string manufactor { get; set; }
-        }
-
-        public class Alien
-        {
-            public Alien()
-            {
-                // constructor
-            }
-            public string planet { get; set; }
+            // constructor
         }
     }
 }
