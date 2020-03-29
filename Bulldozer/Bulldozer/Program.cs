57bab0f2ba28e9de56b50093be3023f871914721
Jackie <JGtheophila19@gmail.com>
2020-03-29 16:01:28 -0400

    use a constructor to create a new bulldozer

--- Bulldozer/Bulldozer/Program.cs
+++ Bulldozer/Bulldozer/Program.cs
@@ -6,20 +6,25 @@ namespace Bulldozer
     {
         public static void Main(string[] args)
         {
-            // create a new bulldozer with the blueprint
-            var dozer = new BullDozer();
+            var dozer = new BullDozer("yellow", "mini");
+            //dozer.color = "yellow";
+            //dozer.size = "mini";
+            Console.WriteLine("Bulldozer 1 is " + dozer.color);
 
-            // assign color and size to the bulldozer
-            dozer.color = "yellow";
-            dozer.size = "mini";
-            Console.WriteLine("Bulldozer is " + dozer.color);
+            var dozer2 = new BullDozer("red", "large");
+            //dozer.color = "red";
+            //dozer.size = "large";
+            Console.WriteLine("Bulldozer 2 is " + dozer2.color);
         }
         
         public class BullDozer
         { 
-            // 2 properties
-
-            // define a color and size
+            // use a constructor to create a new bulldozer
+            public BullDozer(string _color, string _size) {
+                this.color = _color;
+                this.size = _size;
+            }
+            
             public string color { get; set;}
             public string size { get; set; }
         }
