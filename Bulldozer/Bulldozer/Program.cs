bd252f1fd7022ab2f198e6913bf039453a5b5ab0
Jackie <JGtheophila19@gmail.com>
2020-03-29 19:07:56 -0400

    adding 2 numbers together

--- Bulldozer/Bulldozer/Program.cs
+++ Bulldozer/Bulldozer/Program.cs
@@ -7,43 +7,23 @@ namespace Bulldozer
         public static void Main(string[] args)
         {
             var worker = new Workers();
-            worker.Count(4);
-            worker.Count(5);
-            
-            worker.Count(4.5f); // run error
+            var result = worker.AddNumber(1, 2);
+            Console.WriteLine(result.ToString());
         }
 
-        public class Workers
-        {   
-            public Workers()
-            {   
-                // constructor
-            }
-        }
-        
-        public void Count(int number)
+    }
+
+    public class Workers
+    {
+        public Workers()
         {
-            if (Total == null)
-            {
-                Total = 0;
-            }
-            
-            //Total = Total + number;
-            Total = Total + Convert.ToSingle(number);
-            Console.WriteLine(Total.ToString());
-
+            // constructor
         }
-    
-        // ***NOTE*** Don't name your methods the same - exception: putting a different type of variable
 
-        // this method is only called whenever the main program passed a float
-        public void Count(float number)
+        public int AddNumber(int num1, int num2)
         {
-            Total = Total + number;
-            Console.WriteLine(Total.ToString());
+            var result = num1 + num2;
+            return result;
         }
-        
-        // public int Total { get; set; }
-        public float Total { get; set; }
     }
 }
